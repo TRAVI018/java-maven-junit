ce9292881cffeecbaf7c1075f16ec91c0bd97397
TRAVI018 <34261697+TRAVI018@users.noreply.github.com>
2018-02-19 09:51:19 +0530

    Update Hello.java

--- src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -33,6 +33,7 @@ public class Hello {
     public void sayHello(PrintStream printer) {
         for (short i = 0; i < times; i++) {
             printer.println(HELLO);
+            printer.println(Newly added);
         }
     }
 
